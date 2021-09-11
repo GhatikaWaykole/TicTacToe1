0b15300f21df8703938a0f38218d8c2a06be5596
ghatikawaykole <ghatika99@gmail.com>
2021-09-11 08:58:53 +0530

    Tc tac toe problem

--- src/com/example/TicTacToe/TicTacToe.java
+++ src/com/example/TicTacToe/TicTacToe.java
@@ -3,24 +3,18 @@ package com.example.TicTacToe;
 import javafx.scene.transform.Scale;
 
 import java.util.Scanner;
-public class compute{
-    static void ChooseLetter() {
 
-        System.out.println("Which letter you want to choose");
-        String turn ="X";
-        if(turn.equals("X")){
-            turn = "o";
-        }
-        else
-            turn = "X";
-    }
-}
 public class TicTacToe {
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         String[] board = new String[10];
         String turn = "x";
         String winner=null;
+        for(int i=1;i< board.length;i++)
+        {
+            board[i]="";
+
+        }
 
     }
 }
